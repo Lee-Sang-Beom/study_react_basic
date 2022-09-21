d13d75f5ee4292a6a08b43f0997025de1327df3d
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-21 16:20:38 +0900

    use useRef

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Reset } from "styled-reset";
 
 function App() {
 
-  const onSubmit = (e) => {
-    alert('submit');
-  }
-  const onKeyUp = (e) =>{
-    if (e.keyCode === 13){
-      onSubmit();
-    }
+  const countRef = useRef(0);
+  const inputRef = useRef(0);
 
+  const [countState, setCountState] = useState(0);
+
+  useEffect(()=>{
+    inputRef.current.focus();
+  },[]);
+  
+  const upRef = () => {
+    countRef.current += 1;
+    console.log(countRef.current);
+  }
+
+  const focusUp = () => {
+    console.log(inputRef.current.value);
+  }
+
+  const upState = () => {
+    setCountState(countState + 1);
   }
 
   return (
     <React.Fragment>
       <Reset />
       <div>
-        <input onKeyUp={onKeyUp}/>
-        <button onClick={onSubmit}>SUBMIT</button>
+        <p>ref: {countRef.current}</p>
+        <p>state: {countState}</p>
+
+        <button onClick = {upRef}> refUp </button>
+        <button onClick = {upState}> stateUp </button>
+        
+        <input ref={inputRef} type='text' />
+        <button onClick = {focusUp}> submit </button>
+
       </div>
     </React.Fragment>
   );

0dc81c308d110b08cfde50a207836f8213f00688
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-22 12:55:06 +0900

    add body menu

--- src/App.js
+++ src/App.js
@@ -4,15 +4,29 @@ import { Reset } from "styled-reset";
 function App() {
 
   const [title, setTitie] = useState('');
+  const [body, setBody] = useState('');
+
+  const onSubmit = () =>{
+    console.log(title, body);
+  }
   return (
     <React.Fragment>
       <Reset />
       <div className="container">
+        {/* title */}
         <div className="mb-3">
           <label className="form-label">TITLE</label>
-          <input className="form-control" value={title} onChange={(e)=>{setTitie(e.target.value); console.log(e.target.value)}}/>
+          <input className="form-control" value={title} onChange={(e)=>{setTitie(e.target.value);}}/>
         </div>
-        <button className ="btn btn-primary">POST</button>
+
+        {/* body */}
+        <div className="mb-3">
+          <label className="form-label">BODY</label>
+          <textarea className="form-control" rows={15} value={body} onChange={(e)=>{setBody(e.target.value)}}></textarea>
+        </div>
+
+        {/* post */}
+        <button className ="btn btn-primary" onClick={onSubmit} >POST</button>
       </div>
       
     </React.Fragment>

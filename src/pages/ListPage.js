e35e42227aaf1a34f6822b29177486517da6998a
Lee-Sang-Beom <tkdqja975@naver.com>
2022-09-25 14:09:16 +0900

    use useEffect in Listpage

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,5 +1,28 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+
 export default function ListPage(){
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=>{
+        getPosts();
+    }, [])
+
+    const getPosts = () => {
+        axios.get('http://localhost:3001/posts')
+        .then((res) => setPosts(res.data))
+    }
+
+    
     return (
-        <>ListPage</>
+        <>
+        {posts && posts.map((post) => (
+           <div>
+                {post.title}
+                {post.body}
+           </div> 
+        ))}
+        </>
     )
 }
